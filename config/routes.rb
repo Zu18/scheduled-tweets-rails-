db38826ea1df2d802f1a853316e733c4cd2e5ba9
Zu18 <zuhra.galieva.2014@gmail.com>
2022-08-30 10:27:05 +0100

    Created a Sign Up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 #GET /about /- is optional
  get "about", to: "about#index"
+
+ get "sign_up", to: "registrations#new"
+ post "sign_up", to: "registrations#create"
   # root page
  root to: "main#index"
 end
